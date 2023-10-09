9ea2de4c4d60885b429302e314b0901b949bec79
Alinakhan7860 <147390497+Alinakhan7860@users.noreply.github.com>
2023-10-09 18:27:25 +0500

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,26 @@
+def add():
+    a=int(input("enter number"))
+    b=int(input("enter number"))
+    c=a+b
+    print (c)
+add()
+def subtract():
+    d=int(input("enter number"))
+    e=int(input("enter number"))
+    f=d-e
+    print (f)
+subtract()
+def multiple():
+    g=int(input("enter number"))
+    h=int(input("enter number"))
+    i=g*h
+    print (i)
+multiple()
+def divide():
+    j=int(input("enter number"))
+    k=int(input("enter number"))
+    l=j/k
+    print (l)
+divide()
+    
+
